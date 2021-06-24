544cb95cb5c558261cbb9d08a58835958ef3e320
qixian.li <qixian.li@dena.com>
2021-06-24 12:02:43 +0800

    http random request

--- server/httpServer/httpTestServer.ts
+++ server/httpServer/httpTestServer.ts
@@ -2,9 +2,13 @@ import express from "express";
 import * as uuid from "uuid";
 import baseServer from "../baseServer";
 import * as http from "http";
+import Chance from "chance";
 
 const app = express();
 const port = 3000;
+const chance = new Chance();
+const errorCode1 = [402, 401, 404];
+const errorCode2 = [0x111, 0x112, 0x113];
 
 let httpServer = http.createServer(app);
 app.setMaxListeners(100);
@@ -19,9 +23,29 @@ app.get('/errorRequest', (req, res) => {
     res.end();
 });
 
-app.get('/errorStatusCode', (req, res) => {
-    res.send(402);
-    res.end();
+app.get('/randomRequest', (req, res) => {
+    const rand = chance.integer({ min: 0, max: 100 })
+    if (rand < 80) {
+        res.send({
+            errorCode: 0,
+            data: {
+                name: "test",
+                time: Date.now(),
+                token: uuid.v4()
+            }
+        });
+        res.end();
+    }
+    else if (rand < 85) {
+        res.send({
+            errorCode: chance.pickone(errorCode2)
+        });
+        res.end();
+    }
+    else if (rand < 95) {
+        res.send(chance.pickone(errorCode1));
+        res.end();
+    }
 });
 
 app.get('/normalRequest', (req, res) => {
@@ -29,7 +53,7 @@ app.get('/normalRequest', (req, res) => {
         errorCode: 0,
         data: {
             name: "test",
-            time:Date.now(),
+            time: Date.now(),
             token: uuid.v4()
         }
     });
